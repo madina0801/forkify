3d5faa7df22ae09f477dd4a55f33eaf17aff594e
Madina <madinatussupova0820@gmail.com>
2023-01-30 09:43:06 +0600

    error and success messages

--- src/js/controller.js
+++ src/js/controller.js
@@ -25,7 +25,7 @@ const controlRecipes = async function () {
     // 2. Rendering a recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    console.log(err);
+    recipeView.renderError();
   }
 };
 
--- src/js/model.js
+++ src/js/model.js
@@ -25,6 +25,6 @@ export const loadRecipe = async function (id) {
     console.log(state.recipe);
   } catch (err) {
     // Temporary error handling
-    console.error(`!!! ${err}`);
+    throw err;
   }
 };
