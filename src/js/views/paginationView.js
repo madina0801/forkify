58574352c30b393b214e067507d92eca97368bad
Madina <madinatussupova0820@gmail.com>
2023-02-02 12:35:44 +0600

    pagination pages

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -5,27 +5,54 @@ class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
 
   _generateMarkup() {
+    const currentPage = this._data.page;
     const numPages = Math.ceil(
       this._data.results.length / this._data.resultsPerPage
     );
     console.log(numPages);
 
     // Page 1 and there are other pages
-    if (this._data.page === 1 && numPages > 1) {
-      return 'page 1, others';
+    if (currentPage === 1 && numPages > 1) {
+      return `
+      <button class="btn--inline pagination__btn--next">
+        <span>Page ${currentPage + 1}</span>
+        <svg class="search__icon">
+          <use href="${icons}#icon-arrow-right"></use>
+        </svg>
+      </button>`
     }
-
+    
     // Last page
-    if (this._data.page === numPages) {
-      return 'last page';
+    if (currentPage === numPages && numPages > 1) {
+      return `
+      <button class="btn--inline pagination__btn--prev">
+        <svg class="search__icon">
+          <use href="${icons}#icon-arrow-left"></use>
+        </svg>
+        <span>Page ${currentPage - 1}</span>
+      </button>
+      `;
     }
     // Other page
-    if (this._data.page < numPages) {
-      return 'other page';
+    if (currentPage < numPages) {
+      return `
+      <button class="btn--inline pagination__btn--next">
+        <span>Page ${currentPage + 1}</span>
+        <svg class="search__icon">
+          <use href="${icons}#icon-arrow-right"></use>
+        </svg>
+      </button>
+      <button class="btn--inline pagination__btn--prev">
+        <svg class="search__icon">
+          <use href="${icons}#icon-arrow-left"></use>
+        </svg>
+        <span>Page ${currentPage - 1}</span>
+      </button>
+      `;
     }
 
     // Page 1 and no other pages
-    return 'only 1 page';
+    return '';
   }
 }
 
