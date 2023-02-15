d199353fc683bf3012124c680084e0f323ff6bd1
Madina <madinatussupova0820@gmail.com>
2023-02-15 09:47:33 +0600

    add and delete bookmark

--- src/js/controller.js
+++ src/js/controller.js
@@ -74,11 +74,23 @@ const controlServings = function(newServings) {
   recipeView.update(model.state.recipe);
 }
 
+const controlAddBookmark = function() {
+  if(!model.state.recipe.bookmarked) {
+    model.addBookmark(model.state.recipe);
+  } else {
+    model.deleteBookmark(model.state.recipe.id);
+  }
+
+  console.log(model.state.recipe);
+  recipeView.update(model.state.recipe);
+}
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHadlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
-init();
 
+init();
